4e017fe3319d2fa9f06379f30b6126855119c06e
auxo86 <auxo86@gmail.com>
2017-06-08 20:49:35 +0800

    web publish的練習 以建立profile的方式 更新過檔案 在檔案或是目錄上按右鍵就可以publish

---
+++ Module14_01/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Module14_01.Startup))]
+namespace Module14_01
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
